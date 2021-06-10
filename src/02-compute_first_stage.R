b530f53aedd090425c2b106ab45acead46e66879
Stefan McCabe <stefanmccabe@gmail.com>
2021-06-10 11:52:05 -0400

    Phis with NORC lists
    This commit incorporates the NORC lists in the calculation of the elite ideal points.

--- src/02-compute_first_stage.R
+++ src/02-compute_first_stage.R
@@ -14,7 +14,6 @@ colnames(mat) <- readLines("../data/panel_elites_mat_colnames.txt")
 rownames(mat) <- readLines("../data/panel_elites_mat_rownames.txt")
 
 mat <- as.matrix(mat)
-non_politician_columns <- c(525:1558)
 
 mat <- mat[, which(colSums(mat) > 1)]
 politicians <- which(str_starts(colnames(mat), "pres|MS|MH"))
@@ -29,38 +28,3 @@ print(mean(mat))
 res <- CA(mat, nd=3, supcol=non_politicians)
 save(res, file="../data/correspondence_analysis.rdata")
 
-## G <- igraph::graph_from_incidence_matrix(mat)
-
-## assert_that(igraph::is_connected(G))
-## assert_that(igraph::is_bipartite(G))
-## target <- which(colnames(mat) == "MS11741301")
-## assert_that(length(target) == 1)
-
-## starts <- list(
-##     alpha = matrix(normalize(log(colSums(mat) + 0.0001))),
-##     beta = matrix(normalize(log(rowSums(mat) + 0.0001))),
-##     w = matrix(rnorm(dim(mat)[2])),
-##     theta = matrix(rnorm(dim(mat)[1])),
-##     gamma = matrix(c(-1))
-## )
-
-## #mat <- ustweet$data
-## #starts <- ustweet$starts
-## priors <- ustweet$priors
-
-## str(starts)
-## str(priors)
-## out <- networkIRT(.y = mat,
-##                    .starts = starts,
-##                    .priors = priors,
-##                    .control = {list(verbose = FALSE,
-##                                     maxit = 3,
-##                                     convtype = 2,
-##                                     thresh = 1e-6,
-##                                     threads = 1
-##                                     )
-##                            },
-##                    .anchor_item = target
-##                    )
-
-## saveRDS(out, file="/net/data/twitter-covid/ideal_points.Rdata")
