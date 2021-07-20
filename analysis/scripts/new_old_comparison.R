21597d772c769b1426a4bb432ca2a7161a595d55
Jon Green <jon.green.440@gmail.com>
2021-07-20 15:32:04 -0400

    news orgs and some prior analysis
    news orgs for adding to matrix

--- analysis/scripts/new_old_comparison.R
+++ analysis/scripts/new_old_comparison.R
@@ -1,68 +1,189 @@
-# checking old vs. new elite ideal points for overlapping MoC
-# (plus how much overlap in elites)
-
+# checking old vs. new elite ideal points for overlapping elites
 library(tidyverse)
 library(tweetscores)
-library(colorblindr)
+theme_jg <-function(){
+  theme_bw()+
+    theme(text = element_text(family = "serif", size = 16),
+          strip.text = element_text(face = "bold"),
+          plot.title = element_text(face = "bold", size = 20))
+}
 
-# get old tweetscores
-old_elites <- data.frame(handle = tolower(tweetscores::refdataCA$colnames),
-                         ideal_point = tweetscores::refdataCA$colcoord[,1])
+new_tweetscores <- readr::read_tsv("~/Desktop/GitHub/new-tweetscores/data/elites_combined_with_phi.tsv")
 
-# read in new ideal points
-new_elites <- readr::read_tsv("~/Desktop/GitHub/new-tweetscores/data/elites_combined_with_phi.tsv")
+data(refdataCA)
+head(refdataCA)
 
-# how many accounts in the new sample are also in the old sample?
-new_elites %>%
-  mutate(handle = tolower(screen_name)) %>%
-  left_join(old_elites, 
-            by = "handle") %>%
-  mutate(has_old_ideal_point = as.numeric(!is.na(ideal_point))) %>%
-  group_by(source, has_old_ideal_point) %>%
-  summarise(n = n()) %>%
-  group_by(source) %>%
-  mutate(prop = n/sum(n))
+old_tweetscores <- data.frame(handle = tolower(refdataCA$colnames),
+                              tweetscore = refdataCA$colcoord[,1])
 
-# how much nominal agreement among elites that overlap?
+old_tweetscores %>% 
+  arrange(desc(tweetscore)) %>%
+  mutate(index = 1:n()) %>%
+  mutate(lab = ifelse(index %in% seq(from = 1, to = nrow(old_tweetscores), by = 20),
+                      handle, NA)) %>%
+  ggplot()+
+  geom_point(aes(x = tweetscore, y = index))+
+  geom_text(aes(x = tweetscore + .25,
+                y = index, label = lab))+
+  #scale_color_OkabeIto(name = "Community")+
+  labs(x = "TweetScore",
+       y = "Rank",
+       title = "TweetScores (2015)",
+       subtitle = "Every 20th elite labeled",
+       caption = "From Barbera, et al (2015)")+
+  theme_jg()
+
 nominal_agreement <- 
-  new_elites %>%
-  mutate(handle = tolower(screen_name)) %>%
-  left_join(old_elites, 
-            by = "handle") %>%
-  mutate(has_old_ideal_point = as.numeric(!is.na(ideal_point))) %>%
-  ggplot(aes(x = phi, y = ideal_point,
-             col = (source == "members_of_congress")))+
+  new_tweetscores %>%
+  mutate(handle = tolower(screen_name),
+         elite_type = case_when(moc_117 == 1 ~ "Current Member of Congress",
+                                moc_116 == 1 ~ "Recent Member of Congress",
+                                pundit == 1 ~ "Pundit (Green and Masket 2021)",
+                                covid_elite == 1 ~ "Covid Elite (Gallagher et al 2021)",
+                                president == 1 ~ "Current or Former President",
+                                media == 1 ~ "Media Organization",
+                                candidate == 1 ~ "Political Candidate",
+                                governor == 1 ~ "Governor",
+                                official == 1 ~ "Government Official")) %>%
+  left_join(old_tweetscores, by = "handle") %>%
+  ggplot(aes(x = phi, y = tweetscore, col = elite_type))+
   geom_point()+
   geom_abline(slope = 1, intercept = 0)+
-  scale_color_OkabeIto(name = "Member of Congress?")+
+  scale_color_OkabeIto(name = "Elite Type")+
   labs(title = "Ideal point agreement: New estimate vs. tweetScore",
        subtitle = "Among elites with estimates in both samples",
        x = "Phi (2021)",
-       y = "tweetScore (2015)")+
-  theme_bw()
+       y = "tweetScore (2015)",
+       caption = "Referenece line represents y = x")+
+  theme_jg()
 ggsave(nominal_agreement, file = "~/Desktop/GitHub/new-tweetscores/analysis/figures/nominal_agreement_overlap.png", width = 8, height = 4)
+
+# how much overlap?
+new_tweetscores %>%
+  mutate(handle = tolower(screen_name),
+         elite_type = case_when(moc_117 == 1 ~ "Current Member of Congress",
+                                moc_116 == 1 ~ "Recent Member of Congress",
+                                pundit == 1 ~ "Pundit (Green and Masket 2021)",
+                                covid_elite == 1 ~ "Covid Elite (Gallagher et al 2021)",
+                                president == 1 ~ "Current or Former President",
+                                media == 1 ~ "Media Organization",
+                                candidate == 1 ~ "Political Candidate",
+                                governor == 1 ~ "Governor",
+                                official == 1 ~ "Government Official")) %>%
+  left_join(old_tweetscores, by = "handle") %>%
+  filter(!is.na(tweetscore)) %>% nrow()
 
 # how much rank-order agreement among elites who overlap?
 rank_agreement <- 
-  new_elites %>%
-  mutate(handle = tolower(screen_name)) %>%
-  left_join(old_elites, 
-            by = "handle") %>%
-  filter(!is.na(ideal_point)) %>%
+  new_tweetscores %>%
+  mutate(handle = tolower(screen_name),
+         elite_type = case_when(moc_117 == 1 ~ "Current Member of Congress",
+                                moc_116 == 1 ~ "Recent Member of Congress",
+                                pundit == 1 ~ "Pundit (Green and Masket 2021)",
+                                covid_elite == 1 ~ "Covid Elite (Gallagher et al 2021)",
+                                president == 1 ~ "Current or Former President",
+                                media == 1 ~ "Media Organization",
+                                candidate == 1 ~ "Political Candidate",
+                                governor == 1 ~ "Governor",
+                                official == 1 ~ "Government Official")) %>%
+  left_join(old_tweetscores, by = "handle") %>%
+  filter(!is.na(tweetscore)) %>%
   arrange(desc(phi)) %>%
   mutate(phi_rank = 1:n()) %>%
-  arrange(desc(ideal_point)) %>%
+  arrange(desc(tweetscore)) %>%
   mutate(tweetscore_rank = 1:n()) %>%
   ggplot(aes(x = phi_rank, y = tweetscore_rank,
-             col = (source == "members_of_congress")))+
+             col = elite_type))+
   geom_point()+
   geom_abline(slope = 1, intercept = 0)+
-  scale_color_OkabeIto(name = "Member of Congress?")+
+  scale_color_OkabeIto(name = "Elite Type")+
   labs(title = "Ideal point rank agreement: New estimate vs. tweetScore",
        subtitle = "Among elites with estimates in both samples",
        x = "Phi Rank (2021)",
        y = "tweetScore Rank (2015)",
-       caption = "Refernece line represents y = x")+
-  theme_bw()
+       caption = "Referenece line represents y = x")+
+  theme_jg()
 ggsave(rank_agreement, file = "~/Desktop/GitHub/new-tweetscores/analysis/figures/rank_agreement_overlap.png", width = 8, height = 4)
+
+
+# for whom is there disagreement?
+diff_df <- new_tweetscores %>%
+  mutate(handle = tolower(screen_name),
+         elite_type = case_when(moc_117 == 1 ~ "Current Member of Congress",
+                                moc_116 == 1 ~ "Recent Member of Congress",
+                                pundit == 1 ~ "Pundit (Green and Masket 2021)",
+                                covid_elite == 1 ~ "Covid Elite (Gallagher et al 2021)",
+                                president == 1 ~ "Current or Former President",
+                                media == 1 ~ "Media Organization",
+                                candidate == 1 ~ "Political Candidate",
+                                governor == 1 ~ "Governor",
+                                official == 1 ~ "Government Official")) %>%
+  left_join(old_tweetscores, by = "handle") %>%
+  filter(!is.na(tweetscore) & !is.na(phi)) %>%
+  arrange(phi) %>%
+  mutate(phi_rank = 1:n()) %>%
+  arrange(tweetscore) %>%
+  mutate(tweetscore_rank = 1:n()) %>%
+  mutate(diffrank = abs(tweetscore_rank - phi_rank)) %>%
+  arrange(diffrank)
+
+movement_plot <-diff_df %>%
+  arrange(desc(diffrank)) %>%
+  slice(1:100) %>%
+  dplyr::select(handle, elite_type, phi, tweetscore, phi_rank, tweetscore_rank, diffrank) %>%
+  ggplot(aes(x = fct_rev(fct_inorder(handle)),
+             xend = fct_rev(fct_inorder(handle)),
+             y = tweetscore_rank,
+             yend = phi_rank,
+             col = elite_type))+
+  geom_segment(arrow = arrow(length = unit(.05, "inches")))+
+  coord_flip()+
+  scale_color_OkabeIto(name = "Elite Type")+
+  labs(x = "Elite", y = "Rank\nRight = More Conservative",
+       title = "Rank order movement among overlapping elites",
+       subtitle = "Arrows from tweetScore (2015) rank to phi (2021) rank\nTop 100 of 335 overlapping elites with largest movement shown")+
+  theme_jg()
+ggsave(movement_plot, file = "~/Desktop/GitHub/new-tweetscores/analysis/figures/movement_plot.png", width = 10, height = 20)
+
+
+stability_plot <- 
+  diff_df %>%
+  arrange(diffrank) %>%
+  slice(1:100) %>%
+  dplyr::select(handle, elite_type, phi, tweetscore, phi_rank, tweetscore_rank, diffrank) %>%
+  ggplot(aes(x = fct_rev(fct_inorder(handle)),
+             xend = fct_rev(fct_inorder(handle)),
+             y = tweetscore_rank,
+             yend = phi_rank,
+             col = elite_type))+
+  geom_segment(arrow = arrow(length = unit(.05, "inches")))+
+  coord_flip()+
+  scale_color_OkabeIto(name = "Elite Type")+
+  labs(x = "Elite", y = "Rank\nRight = More Conservative",
+       title = "Rank order movement among overlapping elites",
+       subtitle = "Arrows from tweetScore (2015) rank to phi (2021) rank\nBottom 100 of 335 overlapping elites with smallest movement shown")+
+  theme_jg()
+ggsave(stability_plot, file = "~/Desktop/GitHub/new-tweetscores/analysis/figures/stability_plot.png", width = 10, height = 20)
+
+
+diff_df %>%
+  arrange(diffrank) %>%
+  summarise(mean_movement = mean(diffrank),
+            median_movement = median(diffrank),
+            sd_movement = sd(diffrank))
+
+
+diff_dist <- 
+  diff_df %>%
+  ggplot(aes(x = diffrank))+
+  geom_histogram()+
+  geom_vline(xintercept = median(diff_df$diffrank), lty = "solid", col = "red")+
+  geom_vline(xintercept = mean(diff_df$diffrank), lty = "dashed", col = "red")+
+  labs(title = "Distribution of Rank Movement",
+       subtitle = "Mean (dashed) and median (solid) shown with red refernce lines",
+       x = "Absolute Value Movement in Rank (of 335 Overlapping Elites)\n2015-2021",
+       y = "Count")+
+  theme_jg()
+ggsave(diff_dist, file = "~/Desktop/GitHub/new-tweetscores/analysis/figures/diff_dist.png", width = 10, height = 6)
+
 
---
+++ snowball_news_orgs.R
@@ -0,0 +1,130 @@
+# snowball sample news media accounts
+library(tidyverse)
+library(rtweet)
+
+tw_token <- 
+  rtweet::create_token(
+    app = "jigapp",
+    consumer_key = "c2KT4wRTPCM08wCExxD0gJ4AK",
+    consumer_secret = "NCQZ3z7m4EsdzWbEsDQdlNbm0HCTw7HbAjLLcy5IpRjuyKkUzB",
+    access_token = "493387905-Oayoy9RMxNArfUyRdRXf3GqvDJKcMqVgAMfiuEoJ",
+    access_secret = "2Rg5XEvN5HtQCwztnJ513aiIf1NT1FeSTh6HIyDjqRn9V"
+  )
+
+check.limits <- function(){
+  rl <- rtweet::rate_limits()
+  limits <- rl[rl$limit > rl$remaining,]
+  return(limits)
+}
+
+seed.orgs <- c("nytimes","foxnews","breitbartnews","washingtonpost",
+               "nro","latimes","realdailywire","newrepublic",
+               "rollingstone","nymag","voxdotcom",
+               "dailycaller","thehill","realclearnews",
+               "theeconomist","msnbc","cnn","cnnbrk",
+               "cbsnews","abcnews","nbcnews")
+
+keystring <- "news|politics|opinion"
+
+snowfunc <- function(seed, keys){
+  message("get initial left lists...")
+  
+  init.list.ids <- lapply(seed, function(x){
+    lists <- rtweet::lists_memberships(x)
+    list.ids <- lists$list_id[grep(keystring, lists$name, ignore.case = T)]
+    return(list.ids)
+  })
+  
+  lists <- unique(unlist(init.list.ids))
+  
+  message("get snowballed accts...")
+  
+  list.members <- bind_rows(lapply(lists, function(x){
+    rtweet::lists_members(x)
+  }))
+  
+  nlist <- list.members %>% count(screen_name) %>% arrange(desc(n))
+  
+  list_trunc <- list.members %>% 
+    left_join(nlist, by = "screen_name") %>%
+    arrange(desc(n)) %>%
+    filter(!duplicated(screen_name)) %>%
+    mutate(prop = n/length(lists)) %>%
+    filter(prop >= .05)
+
+  users_2 <- tolower(unique(list_trunc$screen_name[-which(tolower(list_trunc$screen_name) %in% seed)]))
+  
+  c <- check.limits() %>%
+    filter(grepl('lists/memberships', query))
+  
+  i <- 0
+  
+  message("get snowballed list ids 2...")
+  
+  if(nrow(c) > 0){
+    pauses <- seq(from = c$remaining - 1, to = length(users_2), by = 75)
+  }else{
+    pauses <- seq(from = 74, to = length(users_2), by = 75)
+  }
+  
+  list.ids2 <- lapply(users_2, function(x){
+    i <<- i+1
+    if(i %in% pauses){
+      message(paste0("pausing to re-up rate limit, ", round(i / length(users_2), 3)*100, "% done..."))
+      Sys.sleep(1000)
+    }
+    lists <- rtweet::lists_memberships(x)
+    list.ids <- lists$list_id[grep(keys, lists$name, ignore.case = T)]
+    return(list.ids)
+  })
+  
+  lists_2 <- unique(unlist(list.ids2))
+  
+  # checking rate limits
+  c <- check.limits() %>%
+    filter(query == "lists/members")
+  
+  i <- 0
+  
+  if(nrow(c) > 0 & length(lists_2) > 900){
+    pauses <- seq(from = c$remaining - 1, to = length(lists_2), by = 900)
+  }
+  if(nrow(c) == 0  & length(lists_2) > 900) {
+    pauses <- seq(from = 900, to = length(lists_2), by = 900)
+  }else{
+    pauses <- 900
+  }
+  
+  message("get snowballed accts 2...")
+  
+  list.members2 <- bind_rows(lapply(lists_2, function(x){
+    i <<- i+1
+    if(i %in% pauses){
+      message(paste0("pausing to re-up rate limit, ", round(i / length(lists_2), 3)*100, "% done..."))
+      Sys.sleep(920)
+    }
+    rtweet::lists_members(x)
+  }))
+  
+  nlist2 <- list.members2 %>% count(screen_name) %>% arrange(desc(n))
+  
+  list_trunc2 <- list.members2 %>% 
+    left_join(nlist2, by = "screen_name") %>%
+    arrange(desc(n)) %>%
+    filter(!duplicated(screen_name)) %>%
+    mutate(prop = n / length(lists_2)) %>%
+    filter(prop >= .05)
+  
+  list_stack <- bind_rows(list_trunc, 
+                          list_trunc2) %>%
+    filter(!duplicated(screen_name))
+
+  return(list_stack)
+}
+
+news_snowball <- snowfunc(seed = seed.orgs, keys = keystring)
+
+write.csv(news_snowball, file = "~/Desktop/GitHub/new-tweetscores/data/snowball_news_orgs_prescrub.csv")
+
+
+
