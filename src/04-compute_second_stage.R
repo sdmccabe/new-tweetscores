17b10105885e8691265b373adb4a27fd7cd1271c
Stefan McCabe <stefanmccabe@gmail.com>
2021-07-22 16:15:36 -0400

    refactor theta computation

--- src/04-compute_second_stage.R
+++ src/04-compute_second_stage.R
@@ -5,18 +5,17 @@ library(patchwork)
 # Pablo's [2020 code](https://github.com/pablobarbera/twitter_ideology/tree/master/2020-update) 
 # threw everything onto BigQuery, but instead we'll keep everything in matrix form.
 
-
 mat <- readMM("../data/panel_elites_mat.mtx")
 
 colnames(mat) <- readLines("../data/panel_elites_mat_colnames.txt")
 rownames(mat) <- readLines("../data/panel_elites_mat_rownames.txt")
 
-elites <- read_tsv('../data/elites_combined_with_phi.tsv', col_types="fcccdddddddcc")
+elites <- read_tsv('../data/elites_combined_with_phi.tsv', col_types="ccciiiiiiiiinnic")
 elites <- filter(elites, meta_id %in% colnames(mat))
 
 
 # We do some pre-processing to take the $\phi$s and align them with the following matrix.
-num_follow <- rowSums(mat)
+# num_follow <- rowSums(mat)
 
 phi <- elites$phi
 names(phi) <- elites$meta_id
@@ -28,17 +27,21 @@ phi_2 <- elites$phi_2
 names(phi_2) <- elites$meta_id
 phi_2 <- phi_2[!duplicated(names(phi_2))]
 # re-align phi with column indices
-phi_2 <- phi[match(colnames(mat), names(phi_2))]
+phi_2 <- phi_2[match(colnames(mat), names(phi_2))]
 
-# We keep the binary following matrix but also generate a weighted version, 
-# replacing the 1s in the binary matrix with the appropriate $\phi$. 
-phi_mat <- t(t(mat) * phi)
-phi_2_mat <- t(t(mat) * phi_2)
 
-# Once we've done that, the $\theta$s are effectively the row means 
-# (ignoring the non-zero elements) of this matrix.
-thetas <- rowSums(phi_mat) / num_follow
-theta_2s <- rowSums(phi_2_mat) / num_follow
+# construct adjacency list representation from
+# incidence matrix
+adj <- apply(mat, 1, function(x) names(which(x == 1)))
+
+# use adjacency list to compute follow count and thetas
+# The $\theta$s are effectively the row means of the adjacency list.
+num_follow <- sapply(adj, length)
+thetas <- sapply(adj, function(x) mean(phi[x], na.rm=T))
+theta_2s <- sapply(adj, function(x) mean(phi_2[x], na.rm=T))
+
+#thetas <- rowSums(phi_mat) / num_follow
+#theta_2s <- rowSums(phi_2_mat) / num_follow
 
 # We bring in side information from the voter file to validate the same way Pablo did.
 vf <- read_csv("~/tmp/TSmart-cleaner-Oct2017-rawFormat.csv", col_types = c(twProfileID="c", tsmart_partisan_score="n"))
